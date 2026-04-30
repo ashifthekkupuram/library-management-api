5cbd7bf65b0ec6cdf34254dbd3ede9022dfb6f95
Ashif Thekkupuram <ashifhussain4548@gmail.com>
2026-05-01 02:15:27 +0530

    fix env config

--- env.ts
+++ env.ts
@@ -43,6 +43,6 @@ try {
   throw e;
 }
 
-export const isDev = () => (env.NODE_ENV = "development");
-export const isTest = () => (env.NODE_ENV = "test");
-export const isProd = () => (env.NODE_ENV = "production");
+export const isDev = () => (env.NODE_ENV === "development");
+export const isTest = () => (env.NODE_ENV === "test");
+export const isProd = () => (env.NODE_ENV === "production");
